ea456f524ad9b355a4bc17288b411a250b690a29
jrahman91 <Jafry.Rahman@gmail.com>
2021-03-01 14:13:37 -0500

    added javaPartTwo.java

---
+++ javaPartTwo.java
@@ -0,0 +1,55 @@
+
+public class javaPartTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// IF ELSE STATEMENTS
+
+		// if (27 > 30) {System.out.println("27 is greater than 30"); }
+		// else { System.out.println("27 is not greater than 30");}
+
+		// int a= 30;
+		// int b= 40;
+		// if(a>b) { System.out.println("30 is greater than 40");}
+		// else { System.out.println("30 is not greater than 40");}
+
+		// int time = 1500;
+		// if (time > 1700) {
+		// System.out.println("Good Evening");
+		// } else
+		// {System.out.println("Good Night");
+		// }
+
+		// loops
+
+		// i=0 so since i will print increasing by 1, up to the 5 index.
+		// index= (0,1,2,3,4)
+		// 0 is the FIRST in the index, then 1,2,3,4. 4 being the FIFTH
+
+		// for (int x=3; x<10; x++) { System.out.println(x);}
+
+		// in this example you can increase by 2
+		// for (int x = 2; x<10; x= x+2) {System.out.println(x);}
+
+		// BOOLEANS
+		// int x= 5;
+		// int y= 7;
+		// System.out.println(5>7);
+
+		// boolean isJavaFun = true;
+		// boolean isTodayMonday= false;
+		// System.out.println(isJavaFun);
+		// System.out.println(isTodayMonday);
+
+		// ARRAY LIST allows you to store multiple values in a single variable
+
+		String[] cars = { "BMW", "Mercedes", "Porsche", "Audi" };
+		System.out.println(cars[0]);
+		// substituing a value
+		cars[3] = "Bentley";
+		System.out.println(cars[3]);
+
+	}
+
+}
